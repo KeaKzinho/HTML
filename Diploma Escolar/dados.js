2675e447927b77c269fa2084ef03793fd133e23f
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2023-12-04 21:34:32 -0300

    Diploma automático por preenchimento de dados.
    Versão final.
     - Diploma concluído.

--- Diploma Escolar/dados.js
+++ Diploma Escolar/dados.js
@@ -3,6 +3,8 @@ window.onload = function (){
     const NOME = urlParams.get("nome")
     const IDADE = urlParams.get("idade")
     const SEXO = urlParams.get("sexo")
+    const CPF = urlParams.get("cpf") 
+    const DISCIPLINA = urlParams.get("disciplina")
 
-    diploma.innerHTML = `A Reitora da Proz Educação, no uso de suas atribuições e tendo em vista a conclusão do curso de ${NOME}, de ${IDADE} anos, do sexo ${SEXO} da escola Proz Educação, completou o curso "Análise de Desenvolvimento de Sistemas" com sucesso.`
+    diploma.innerHTML = `A Reitora da José Fonseica Educação, no uso de suas atribuições e tendo em vista a conclusão do Curso de ${DISCIPLINA}, com o formando ${NOME}, com a idade de ${IDADE} anos, do CPF ${CPF}, do sexo ${SEXO}, completou o curso com sucesso.`
 }
